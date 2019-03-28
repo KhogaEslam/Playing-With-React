1e5eec80c89f768cd3ae7de7623c6ecba1a9c54f
Eslam Gamal Khoga <KhogaEslam@Gmail.com>
2019-03-28 17:39:47 +0200

    Branch:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 // import logo from './logo.svg';
-import './App.css';
-import AntTable from './AntTable/AntTable';
-import AntTableDragDropColumn from './AntTable/AntTableDragDropColumn';
-import DragMergingColumn from './AntTable/DragMergingColumn';
+import './App.css'
+import AntTable from './AntTable/AntTable'
+import AntTableBigNumberOfColumns from './AntTable/AntTableBigNumberOfColumns'
+import AntTableDragDropColumn from './AntTable/AntTableDragDropColumn'
+import DragMergingColumn from './AntTable/DragMergingColumn'
 import AntHeader from './AntTable/AntHeader'
 import AntHeaderCustomIcon from './AntTable/AntHeaderCustomIcon'
 import AntHeaderMultiLevels from './AntTable/AntHeaderMultiLevels'
@@ -12,43 +13,110 @@ import AntWithHighChart from './AntTable/AntWithHighChart'
 
 import DNDScrollZone from './DND/DNDScrollZone'
 
-const AntTableExamples = (props) => (
-    <div>
-      <AntTable />
-      <hr></hr>
-      <AntTableDragDropColumn / >
-      <hr></hr>
-      <DragMergingColumn />
-      <hr></hr>
-      <AntHeader />
-      <hr></hr>
-      <AntHeaderCustomIcon />
-      <hr></hr>
-      <AntHeaderMultiLevels />
-      <hr></hr>
-      <AntHeaderCustomOptionsMenu />
-      <hr></hr>
-      <AntWithHighChart />
-      <hr></hr>
-    </div>
-  );
+import BeautifulDNDWithTree from './DND/BeautifulDND/BeautifulDNDWithTree/BeautifulDNDWithTree'
 
-const SmoothDNDWithVirtualList = (props) => (
-    <div>
-      <hr></hr>
+import VerticalList from './DND/BeautifulDND/Basic/VerticalList'
+import HorizontalList from './DND/BeautifulDND/Basic/HorizontalList'
+import WithFunctionComponents from './DND/BeautifulDND/Basic/WithFunctionComponents'
+import BetweenTwoLists from './DND/BeautifulDND/Basic/BetweenTwoLists'
+
+import Virtualized from './SortableHOC/Virtualized'
+import TableWithSortableColumns from './SortableHOC/TableWithSortableColumns'
+
+import VirtualizedSelect from './VirtualizedSelect/VirtualizedSelect'
+// import ConfigurableExample from './VirtualizedSelect/MyConfigurableExample'
+
+const AntTableExamples = props => (
+  <div>
+    <hr />
+    <AntTableBigNumberOfColumns />
+    <hr />
+    {
+      /*
+    <AntTable />
+    <hr />
+    <AntTableDragDropColumn />
+    <hr />
+    <DragMergingColumn />
+    <hr />
+    <AntHeader />
+    <hr />
+    <AntHeaderCustomIcon />
+    <hr />
+    <AntHeaderMultiLevels />
+    <hr />
+    <AntHeaderCustomOptionsMenu />
+    <hr />
+    <AntWithHighChart />
+    <hr />
+    */
+    }
+  </div>
+)
+
+const DND = props => (
+  <div>
+    <hr />
+    <VerticalList />
+    <hr />
+    <HorizontalList />
+    <hr />
+    <WithFunctionComponents />
+    <hr />
+    <BetweenTwoLists />
+    <hr />
+    {
+      /*
+      <BeautifulDNDWithTree />
+      <hr />
       <DNDScrollZone />
-    </div>
-  );
+    <hr />
+    */
+    }
+  </div>
+)
+
+const VirtualizedSortableHOCTrial = props => (
+  <div>
+    <hr />
+    <Virtualized />
+    <hr />
+    <TableWithSortableColumns />
+    <hr />
+  </div>
+)
+
+const VirtualizedSelectTrial = props => (
+  <div>
+    <hr />
+    <VirtualizedSelect />
+    <hr />
+    {
+      /*
+      <ConfigurableExample />
+    <hr />
+    */
+    }
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmoothDNDWithVirtualList />
-        <AntTableExamples />
+        <h1>Testing Area</h1>
+        <hr />
+        <DND />
+        {
+          /*
+          <AntTableExamples />
+          < VirtualizedSelectTrial / >
+          <VirtualizedSortableHOCTrial />
+        */
+        }
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
